30d21c8b23390b13e714f8e9abd0e17c4e4ef6ab
nelsonjrm <nelson.727@gmail.com>
2024-02-06 10:39:44 -0300

    Projeto criado

---
+++ src/script.js
@@ -0,0 +1,13 @@
+let ehPlatina = false
+let oHeroi = "SuperJs"
+let xpPlatina = 7.001 
+let nivelPlatina = "Platina"
+
+if(ehPlatina){
+    xpPlatina == 7.000
+}
+else{
+    console.log("O Herói " + oHeroi + " Está no Nível " + nivelPlatina)
+
+}
+
